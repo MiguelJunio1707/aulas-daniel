4aef229ff8f20983dd6e28330c3c13c34d4e6bbe
MiguelJunio1707 <migueljunio1707@gmail.com>
2025-10-15 22:07:21 -0300

    v1

--- Aula - 10 - javascrip/index.js
+++ Aula - 10 - javascrip/index.js
@@ -81,14 +81,130 @@
 //  <= -> menor ou igual a
 // >= -> maior ou igual a 
 
-var valor1 = "10";
-var valor2 = "20";
+// var valor1 = "10";
+// var valor2 = "20";
 
-document.write(`<h1> funções javascript </h1>`);
+// document.write(`<h1> funções javascript </h1>`);
 
-if(valor1 != valor2){
-    document.write(`o valor ${valor1} é igual a o ${valor2}<br>`);
-}
-else{
-    document.write(`o ${valor1} é diferente do ${valor2}`);
-}
+// if(valor1 != valor2){
+//     document.write(`o valor ${valor1} é igual a o ${valor2}<br>`);
+// }
+// else{
+//     document.write(`o ${valor1} é diferente do ${valor2}`);
+// }
+
+// funções
+// function saudacao(nome){
+//     // document.write(`Hello World ${nome}`);
+//     return `Bom dia ${nome}`
+// }
+// const mensagem = saudacao('Miguel');
+// document.write(mensagem);
+
+// function soma(x, y){
+//     const resultado = x + y;
+//     return resultado;
+// }
+// function subtração(x, y){
+//     const resultado = x - y;
+//     return resultado;
+// }
+// function divisão(x, y){
+//     const resultado = x / y;
+//     return resultado;
+// }
+// function multiplicação(x, y){
+//     const resultado = x * y;
+//     return resultado;
+// }
+// console.log(soma(5, 4));
+// console.log(subtração(5, 4));
+// console.log(divisão(5, 4));
+// console.log(multiplicação(5, 4));
+
+// var nota = 5;
+
+// if(nota >= 6){
+//     document.write(`A nota ${nota} <hr>Está aprovado!!`);
+// }
+// else if ((nota < 6) && (nota >= 4)){
+//     document.write(`A nota ${nota} <hr>Foi aprovada por pouco!!`)
+// }
+// else{
+//     document.write(`A nota ${nota} <hr>Não foi aprovada!!`)
+// }
+
+// var chuva = false;
+// var sol = true;
+
+// if(sol){
+//     document.write('vamos a praia');
+// }
+// else if (chuva){
+//     document.write('não vamos');
+// }
+
+// switch key
+
+// var key = 5;
+
+// switch (key) {
+//     case 1:
+//         document.write(`A chave ${key} é da casa.`);        
+//         break;
+//     case 2: 
+//         document.write(`A chave ${key} é do apartamento`);
+//         break;
+//     case 3:
+//         document.write(`A chave ${key} é do carro`);
+//         break;
+//     case 4:
+//         document.write(`A chave ${key} é da moto`);
+//         break
+//     case 5:
+//         document.write(`A chave ${key} é do barco`)
+//         break
+//     default:
+//         document.write(`A chave não corresponde`);
+//         break;
+// }
+
+// var cadeira = {
+//     cor: "preta",
+//     altura: 118,
+//     largura: 74,
+//     profundidade: 64,
+//     peso: 17
+// };
+
+// document.write(`A cor da cadeira é: ${cadeira.cor}.<hr>`);
+// document.write(`A cor da cadeira é: ${cadeira.altura}cm.<hr>`);
+// document.write(`A cor da cadeira é: ${cadeira.largura}cm.<hr>`);
+// document.write(`A cor da cadeira é: ${cadeira.profundidade}cm.<hr>`);
+// document.write(`A cor da cadeira é: ${cadeira.peso}Kg.<hr>`);
+
+// var mesa = new Object();
+// mesa.cor = "Preta";
+
+// document.write(`A cor da mesa é: ${mesa.cor}.<hr>`);
+
+// arrays
+
+var frutas = ["abacate","Maça", "Melancia", "Banana","Manga","Abacaxi","Uva"];
+
+// Contar a quantidade de itens do array
+
+document.write(`Quantidade de frutas: ${frutas.length}<hr>`);
+
+// Acessar a primeira item do meu array
+
+document.write(`A primeira fruta é: ${frutas[3]}<hr>`);
+
+// Acessar o último item da lista 
+
+document.write(`O último item da lista é: ${frutas[frutas.length-1]}<hr>`);
+
+// Ler um array completo
+frutas.forEach(function(item, indice, array){
+    document.write(item, indice, );
+})
